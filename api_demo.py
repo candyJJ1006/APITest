f91f511ac946a6fcf6d433dd07e09d1be3d978a5
candyJJ1006 <13711793826zjj@gmail.com>
2025-04-06 15:12:30 +0800

    add new cases

---
+++ api_demo.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import requests
+
+
+def main():
+    url = "https://www.baidu.com"
+    try:
+        response = requests.get(url).text
+    except Exception as e:
+        return f'错误:{e}'
+    return response
+
+if __name__ == "__main__":
+    result = main()
+    print(result)
